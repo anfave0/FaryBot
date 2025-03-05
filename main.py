6156ad6f42a0748d4a8c21f04d1819d8827253f1
anfave0 <anoushevergeot@gmail.com>
2025-03-05 18:52:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os 
 from dotenv import load_dotenv 
 import asyncio
-from keep_alive import keep_alive
+from utils.keep_alive import keep_alive
 import time 
 import discord
 from discord.ext import commands
